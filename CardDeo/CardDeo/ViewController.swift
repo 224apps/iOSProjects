c6385ac991475894562137ff40ba68a0cb8c94a4
Abdoulaye Diallo <224apps@Abdoulayes-iMac.local>
2019-03-20 08:25:44 -0700

    Initialed the project DogYears

---
+++ CardDeo/CardDeo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardDeo
+//
+//  Created by Abdoulaye Diallo on 3/12/19.
+//  Copyright © 2019 Abdoulaye Diallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
