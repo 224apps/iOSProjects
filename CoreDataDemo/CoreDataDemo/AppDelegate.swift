d4b84bbdc7570148fcd782bb681688ed5592f9ed
224Apps <224apps@gmail.com>
2020-12-01 14:08:28 -0500

    Transforming SwiftUI App protocol to Appelegate

---
+++ CoreDataDemo/CoreDataDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  CoreDataDemo
+//
+//  Created by Abdoulaye Diallo on 12/1/20.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Your code here")
+        return true
+    }
+
+}
--- CoreDataDemo/CoreDataDemo/CoreDataDemoApp.swift
+++ CoreDataDemo/CoreDataDemo/CoreDataDemoApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct CoreDataDemoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct CoreDataDemoApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
