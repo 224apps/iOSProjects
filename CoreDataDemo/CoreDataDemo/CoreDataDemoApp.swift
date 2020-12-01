eb93de624a6dbc99ec26fb824edb1c4a9ae16d6d
224Apps <224apps@gmail.com>
2020-12-01 13:46:55 -0500

    Initial project

---
+++ CoreDataDemo/CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Abdoulaye Diallo on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
