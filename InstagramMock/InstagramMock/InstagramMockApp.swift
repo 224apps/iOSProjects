66d14dcebe6b16727b69a13cc4c4aae97ae4cabe
Abdoulaye <224apps@gmail.com>
2023-04-29 13:59:48 -0400

    Project Setup and organization

---
+++ InstagramMock/InstagramMock/InstagramMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramMockApp.swift
+//  InstagramMock
+//
+//  Created by Abdoulaye Diallo on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
