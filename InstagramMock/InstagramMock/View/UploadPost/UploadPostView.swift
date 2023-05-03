2dbb8af2bdd36ed83bb728cd7014cf15bb16a860
Abdoulaye <224apps@gmail.com>
2023-05-03 13:14:52 -0400

    Building UploadPostView and adding a selected Image

--- InstagramMock/InstagramMock/View/UploadPost/UploadPostView.swift
+++ InstagramMock/InstagramMock/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    //MARK: -
+    @State private var selectedImage: UIImage?
+    @State private var postImage: Image?
+    @State private var captionText: String =  ""
+    @State private var imagePickerPresented = false
+    
+    //MARK: -
     var body: some View {
-        Text("Upload")
+        VStack {
+            
+            if postImage == nil {
+                Button(action: {imagePickerPresented.toggle()}) {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .renderingMode(.template)
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                }
+                .sheet(isPresented: $imagePickerPresented, onDismiss: loadImage) {
+                    ImagePicker(image: $selectedImage)
+                }
+            } else {
+                HStack(alignment: .top) {
+                    if let image = postImage {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 96, height: 96)
+                            .clipped()
+                            .cornerRadius(5)
+                    } else {
+                        Image("jetli")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 96, height: 96)
+                            .clipped()
+                            .cornerRadius(5)
+                    }
+                
+                    TextField("Enter your caption...", text: $captionText)
+                }
+                .padding()
+                
+                Button(action: {}) {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+            }
+            
+            Spacer()
+        }
+        
+        
+    }
+}
+
+//MARK: - Extension
+
+extension  UploadPostView {
+    func loadImage(){
+        guard let selectedIamge = selectedImage else { return }
+        postImage = Image(uiImage: selectedIamge)
     }
 }
 
