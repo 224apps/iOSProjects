04942844d2d4c9b3654c4fe598477337a27583c7
Abdoulaye <224apps@gmail.com>
2023-05-04 12:40:21 -0400

    Setting up Firebase

--- InstagramMock/InstagramMock/InstagramMockApp.swift
+++ InstagramMock/InstagramMock/InstagramMockApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct InstagramMockApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+   
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
